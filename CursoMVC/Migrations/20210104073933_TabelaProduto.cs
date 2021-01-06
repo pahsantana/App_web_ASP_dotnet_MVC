318304cbdc9dc5edc9e11d0f55480e570eac3a77
Paloma Santana <santanagilberto@uol.com.br>
2021-01-06 03:15:03 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Migrations/20210104073933_TabelaProduto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoMVC.Migrations
+{
+    public partial class TabelaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
---
+++ Curso_Test/ctor.cs
@@ -0,0 +1,6 @@
+﻿namespace Curso_Test
+{
+    internal class ctor
+    {
+    }
+}
